ddda91838f80ad208ad4769dcfd7cf1e6018b782
mocrsteel <yannick.thimmesch@gmail.com>
2023-10-24 22:52:19 +0200

    feat: extended tests for internals and finalized TextContext

--- api/tests/test_internals_context.rs
+++ api/tests/test_internals_context.rs
@@ -7,34 +7,43 @@ use common::db::Context;
 
 use diesel::prelude::*;
 
-#[test]
-fn context_setup_and_drop() {
-    let ctx = Context::new("internals");
-    assert!(true);
+mod context {
+    use super::*;
+    use test_log::test;
+    #[test]
+    fn context_setup_and_drop() {
+        let _ctx = Context::new("internals");
+        // assert!(true)
+    }
+
+    #[test]
+    fn second_context() {
+        let _ctx = Context::new("internals");
+        // assert!(true)
+    }
 }
 
-#[test]
-fn second_context() {
-    let ctx = Context::new("internals");
-    assert!(true)
-}
+mod tables {
+    use super::*;
+    use test_log::test;
 
-#[test]
-fn tables() {
-    let mut ctx = Context::new("internals_tables");
-    let conn = &mut ctx.establish_connection();
+    #[test]
+    fn correct_tables_present() {
+        let mut ctx = Context::new("internals_tables");
+        let conn = &mut ctx.establish_connection();
 
-    diesel::sql_query("SELECT * FROM products;")
-        .execute(conn)
-        .unwrap();
+        diesel::sql_query("SELECT * FROM products;")
+            .execute(conn)
+            .unwrap();
 
-    diesel::sql_query("SELECT * FROM storage;")
-        .execute(conn)
-        .unwrap();
+        diesel::sql_query("SELECT * FROM storage;")
+            .execute(conn)
+            .unwrap();
 
-    let false_table_returns_error = diesel::sql_query("SELECT * FROM does_not_exist")
-        .execute(conn)
-        .is_err();
+        let false_table_returns_error = diesel::sql_query("SELECT * FROM does_not_exist")
+            .execute(conn)
+            .is_err();
 
-    assert!(false_table_returns_error);
+        assert!(false_table_returns_error);
+    }
 }
