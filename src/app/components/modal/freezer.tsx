5b59f50ef03c3116a5512a7bba1c2509aaf6a6b4
Yannick Thimmesch <yannick.thimmesch@gmail.com>
2024-07-25 14:59:18 +0200

    feat/building UI - Freezers modal

--- src/app/components/modal/freezer.tsx
+++ src/app/components/modal/freezer.tsx
@@ -1,4 +1,4 @@
-import style from "@styles/action-button.module.scss"
+import style from "@styles/modals.module.scss"
 import { useState } from "react"
 
 /**
@@ -19,6 +19,38 @@ type DrawerInputProps = {
   onDelete: (e: React.MouseEvent<HTMLButtonElement>) => void,
 }
 
+/**
+ * Type to define all possible input errors. Used to access the different error messages 
+ * and to toggle their visibility individually.
+ */
+type FreezerInputErrors = {
+  invalidFreezerName: {
+    active: boolean,
+    message: string,
+  },
+  invalidDrawerName: {
+    // Opted for `boolean[]` to allow individually setting this message
+    // per drawer input field.
+    active: boolean[],
+    message: string,
+  },
+  noDrawers: {
+    active: boolean,
+    message: string,
+  },
+  /*
+  existingFreezerName: {
+    active: false,
+    message: string,
+  }
+  */
+}
+
+/**
+ * 
+ * @param index of drawer, drawer name, method to update the drawer, method to delete the drawer. 
+ * @returns 
+ */
 const DrawerInput = ({ idx, name, updateDrawer, onDelete }: DrawerInputProps) => {
   return (
     <div key={"div-" + idx}>
@@ -39,10 +71,82 @@ const DrawerInput = ({ idx, name, updateDrawer, onDelete }: DrawerInputProps) =>
  */
 export const AddFreezer = (onClose: () => void) => {
   const [freezer, updateFreezer] = useState<freezerState>({ name: "", drawers: [] })
+  const [ inputErrors, setInputErrors ] = useState<FreezerInputErrors>({
+    invalidFreezerName: {
+      active: false,
+      message: "Invalid freezer name. Name should be at least 3 characters long."
+    },
+    invalidDrawerName: {
+      active: false,
+      message: "Invalid drawer name. Name should be at least 3 characters long."
+    },
+    noDrawers: {
+      active: false,
+      message: "Add at least one drawer."
+    }
+  })
 
+  /**
+   * Helper function to check if any error is found in the inputs. Uses `inputError` state object.
+   * @returns boolean
+   */
+  const checkInputisValid = (): boolean => {
+    return Object.values(inputErrors).map(err => err.active).length === 0
+  }
+
+  /**
+   * Checks the different input fields prior to submitting. Sets an error as active 
+   * if an error is found and blocks the submit process.
+   * @param e React.FormEvent
+   */
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
-  }
+    if (e.currentTarget.id === "freezer-add-create") {
+      
+      // No drawers defined
+      if (freezer.drawers.length === 0) {
+        setInputErrors({
+          ...inputErrors,
+          noDrawers: {
+            ...inputErrors.noDrawers,
+            active: true,
+          }
+        })
+      } else {
+        setInputErrors({
+          ...inputErrors,
+          noDrawers: {
+            ...inputErrors.noDrawers,
+            active: false,
+          }
+        })
+      }
+
+      // Invalid freezer name
+      if (freezer.name.length < 3) {
+         setInputErrors({
+          ...inputErrors,
+          invalidFreezerName: {
+            ...inputErrors.invalidFreezerName,
+            active: true,
+          }
+         })
+      } else {
+        setInputErrors({
+          ...inputErrors,
+          invalidFreezerName: {
+            ...inputErrors.invalidFreezerName,
+            active: false,
+          }
+         })
+      }
+      
+    }
+
+    if (checkInputisValid()) {
+      console.log(freezer)
+    }
+   }
 
   const handleUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault()
@@ -99,9 +203,12 @@ export const AddFreezer = (onClose: () => void) => {
     <>
       <h1>Create new freezer</h1>
       <form onSubmit={handleSubmit}>
-        <label>Freezer name
-          <input id={"input-freezer-name"} type={"text"} />
+        <label className={style.mainLabel}>Freezer name
+          <input id={"input-freezer-name"} className={style.mainInput} type={"text"} />
         </label>
+        {inputErrors.noDrawers.active &&
+          <p className={style.invalidInputMessage}>{inputErrors.noDrawers.message}</p>
+        }
         {freezer.drawers.length > 0 &&
           freezer.drawers.map((name, idx) => {
             return (
@@ -109,11 +216,13 @@ export const AddFreezer = (onClose: () => void) => {
             )
           })
         }
-        <button id="btn-add-drawer" className={"btn-negative " + style.centerButton} onClick={addDrawer}>Add drawer</button>
+        <div id="center-btn-container" className={style.centerButton}>
+          <button id="btn-add-drawer" className={"btn btn-negative"} onClick={addDrawer}>Add drawer</button>
+        </div>
       </form>
       <div className={style.modalButtons}>
         <button id={"freezer-add-cancel"} className={"btn btn-negative"} onClick={onClose}>Cancel</button>
-        <button id={"freezer-add-create"} className={"btn btn-primary"} onClick={onClose}>Create</button>
+        <button id={"freezer-add-create"} className={"btn btn-primary"} onClick={handleSubmit}>Create</button>
       </div>
       
     </>
