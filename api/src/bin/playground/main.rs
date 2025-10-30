a46c59ff5a6de1022d0ed63285056fcbd889b39b
Yannick Thimmesch <yannick.thimmesch@gmail.com>
2025-10-31 00:23:42 +0100

    feat: JWT.rs - Claims

--- api/src/bin/playground/main.rs
+++ api/src/bin/playground/main.rs
@@ -1,12 +1,6 @@
-use chrono::{Months, NaiveDate};
-
+use uuid::{Uuid, timestamp::Timestamp};
+use std::time::{ UNIX_EPOCH, SystemTime};
 fn main() {
-    let test_str = "2023-04-01";
-    let date_parsed = NaiveDate::parse_from_str(test_str, "%Y-%m-%d").unwrap();
-    let date_add_months = date_parsed.checked_add_months(Months::new(12)).unwrap();
-
-    dbg!(date_parsed);
-    dbg!(date_add_months);
-
-    println!("playground");
+    let uuid = Uuid::new_v4();
+    println!("UUID: {uuid}");
 }
